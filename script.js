cb7ce74a16b06ca5ea65f91a276929da3d3dcb24
uriah <108307211+IronAntelope@users.noreply.github.com>
2024-05-15 12:26:09 +0930

    Add resize button
    The program will take a prompt from the user when the resize button
    is hit that will change to size of the etch-a-sketch grid

--- script.js
+++ script.js
@@ -1,19 +1,47 @@
-const container = document.querySelector("#container");
+const containerGrid = document.querySelector("#container");
 
-for (let i = 0; i < 16; i++) {
-  const col = document.createElement("div");
-  col.classList.add("col");
+//grid for drawing
+function createGrid(size) {
+  containerGrid.textContent = "";
+
+  for (let i = 0; i < size; i++) {
+    const col = document.createElement("div");
+    col.classList.add("col");
+
+    for (let j = 0; j < size; j++) {
+      const square = document.createElement("div");
+      square.classList.add("square");
+      col.appendChild(square);
+
+      //draw in grid
+      square.addEventListener("mouseover", (e) => {
+        e.target.style.backgroundColor = rndColor();
+      });
+    };
+
+    containerGrid.appendChild(col);
+  };
+};
+createGrid(16);
+
+//user's grid size choice
+const changeSize = document.querySelector("#chSize");
+changeSize.addEventListener("click", (e) => {
+  const size = prompt("How big would you like to resize?");
   
-  for (let j = 0; j < 16; j++) {
-    const square = document.createElement("div");
-    square.classList.add("square");
-    col.appendChild(square);
-    
-    square.addEventListener("mouseover", (e) => {
-        e.target.style.backgroundColor = "red";
-        console.log(e.type + "!");
-    });
+  if(size <= 0 || size > 100) {
+    size = 16;     
   };
   
-  container.appendChild(col);
+  createGrid(size);
+});
+
+//functions
+//rainbow
+function rndColor() {
+  const rndCol = `rgb(${random(255)} ${random(255)} ${random(255)})`;
+  return rndCol;
+};
+function random(number) {
+  return Math.floor(Math.random() * number);
 };
